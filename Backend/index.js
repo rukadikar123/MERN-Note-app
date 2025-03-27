29cafa89282901f946ba0df6caf144b666381887
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-03-27 22:13:43 +0530

    initialize backend with Express, Mongoose, and dotenv

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app=express()
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to mongoDB");
+    
+}).catch((err)=>{
+    console.log(err);
+    
+})
+
+
+
+app.listen(3000, (req, res)=>{
+    console.log("server is running on port 3000");
+    
+})
