5b807c5e58608e1ab9b2e0a936e3f8f3b53ddab2
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-03-28 19:51:33 +0530

    implement note editing, retrieval, deletion, and pinning functionality

--- Backend/Controller/note.controller.js
+++ Backend/Controller/note.controller.js
@@ -27,3 +27,126 @@ export const addNote = async (req, res) => {
     throw new Error(error);
   }
 };
+
+export const editNote=async(req, res)=>{
+    const {noteId}=req.params
+    const note=await Note.findById(noteId)
+    
+
+    if(!note){
+        return res.status(400).json({
+            success:false,
+            message:"note not found"
+        })
+    } 
+
+    if(req.user.id !== note.userId){
+        throw new Error("You can only update your own note")
+    }
+
+    const {title, content, tags,  isPinned}=req.body
+    if(!title && !content && !tags){
+        throw new Error("No changes Provided")
+    }
+
+    try {
+        if(title){
+            note.title=title
+        }
+        if(content){
+            note.content=content
+        }
+        if(tags){
+            note.tags=tags
+        }
+        if(isPinned){
+            note.isPinned=isPinned
+        }
+        await note.save()
+
+        res.status(200).json({
+            success:true,
+            message:"note updated successfully",
+            note
+        })
+
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+
+export const getAllNotes=async(req,res)=>{
+    try {
+        const userId=req.user.id
+    
+        const notes=await Note.find({userId}).sort({isPinned:-1})
+    
+        if(!notes){
+            throw new Error("Notes Not found")
+        }
+    
+        res.status(200).json({
+            success:true,
+            message:"All notes retrieved successfully",
+            notes
+        })
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+
+
+export const deleteNote=async (req, res)=>{
+    const noteId=req.params.noteId
+
+    const note=await Note.findOne({_id:noteId,userId:req.user.id})
+
+    if(!note){
+        throw new Error("Note not found")
+    }
+
+    try {
+         await Note.deleteOne({_id:noteId,userId:req.user.id})   
+
+         res.status(200).json({
+            success:true,
+            message:"note delete successfully   "
+         })
+
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+
+export const updateNotePinned=async(req, res)=>{
+
+    try {
+        const note=await Note.findById(req.params.noteId)
+
+        if(!note)    {
+            throw new Error("Note not found!");
+        }
+
+        if(req.user.id !== note.userId){
+            return res.status(400).json({
+                success:false,
+                message:"you can only update your own note"
+            })
+        }
+        const {isPinned}=req.body
+
+       note.isPinned=isPinned
+       await note.save()
+        res.status(200).json({
+            success:true,
+            message:"note updated successfully",
+            note
+        })
+
+    } catch (error) {
+        throw new Error(error);   
+    }
+}
--- Backend/Routes/note.routes.js
+++ Backend/Routes/note.routes.js
@@ -1,10 +1,14 @@
 import {Router} from 'express'
 import { verifyToken } from '../middlewares/auth.middleware.js'
-import { addNote } from '../Controller/note.controller.js'
+import { addNote, deleteNote, editNote, getAllNotes, updateNotePinned } from '../Controller/note.controller.js'
 
 const router=Router()
 
 router.post("/add", verifyToken, addNote)
-
+router.post("/edit/:noteId", verifyToken,editNote )
+router.get("/all", verifyToken, getAllNotes )
+router.delete("/delete/:noteId", verifyToken, deleteNote )
+router.put("/update-note-pinned/:noteId", verifyToken, updateNotePinned )
+ 
 
 export default router
