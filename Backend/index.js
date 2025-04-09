1f8b1e9ed755ad3408fca2c1bd84bf28e27977f6
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-04-09 21:39:15 +0530

    fix: streamline CORS configuration and ensure Set-Cookie header is passed in nginx

--- Backend/index.js
+++ Backend/index.js
@@ -29,9 +29,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-      origin: ["https://mern-note-app-1-1cgt.onrender.com",
-        "https://mern-note-app-2.onrender.com"
-        ], // frontend domain
+      origin: "https://mern-note-app-1-1cgt.onrender.com", // frontend domain
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
